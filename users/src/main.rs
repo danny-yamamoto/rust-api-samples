25a41df214d7a757930c1691af6036ce876b1d9c
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-11-09 22:16:25 +0000

    Add draft get users

--- users/src/main.rs
+++ users/src/main.rs
@@ -1,37 +1,59 @@
 use std::env;
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
 
+use axum::Json;
+use axum::Router;
+use axum::extract::Query;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get;
 use dotenv::dotenv;
-use sqlx::{SqlitePool, Row};
+use serde::Deserialize;
+use serde::Serialize;
+
+use sqlx::SqlitePool;
+use sqlx::FromRow;
+
+#[derive(Serialize, FromRow)]
+struct User {
+    user_id: i64,
+    email_address: Option<String>,
+    created_at: Option<i64>,
+    deleted: Option<i64>,
+    settings: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct UserQuery {
+    user_id: i64
+}
 
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    println!("hello");
+async fn main() {
+    let app = Router::new().route("/users", get(user_handler));
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+}
+
+async fn user_handler(Query(query): Query<UserQuery>) -> impl IntoResponse {
+    let search_user_id = query.user_id;
     dotenv().ok();
     let key = "DATABASE_URL";
-    let mut db_url = String::from("sqlite:./local.db");
-    match env::var(key) {
-        Ok(val) => {
-            db_url = val;
-            println!("success: {}", db_url);
-        },
-        Err(error) => {println!("failed: {}", error);},
-    }
+    let db_url = env::var(key).unwrap_or_else(|_| String::from("sqlite:./local.db"));
     println!("db_url: {}", db_url);
-    let pool = SqlitePool::connect(&db_url).await?;
 
-    let rows = sqlx::query("SELECT * FROM users")
-    .fetch_all(&pool)
-    .await?;
-    // 取得したデータを出力
-    for row in rows {
-        let user_id: i32 = row.get("user_id");
-        let email_address: String = row.get("email_address");
-        let created_at: i64 = row.get("created_at");
-        let deleted: i32 = row.get("deleted");
-        let settings: String = row.get("settings");
-        
-        println!("User ID: {}, Email: {}, Created At: {}, Deleted: {}, Settings: {}",
-                user_id, email_address, created_at, deleted, settings);
+    match SqlitePool::connect(&db_url).await {
+        Ok(pool) => {
+            match sqlx::query_as!(User, "SELECT user_id, email_address, created_at, deleted, settings FROM users WHERE user_id = ?", search_user_id)
+                .fetch_optional(&pool)
+                .await
+            {
+                Ok(user) => (StatusCode::OK, Json(user)),
+                Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, Json(None::<User>)),
+            }
+        },
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, Json(None::<User>)),
     }
-    Ok(())
 }
