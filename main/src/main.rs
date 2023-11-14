433e04365a934452a3f911b905518d6f64bf3f38
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-11-14 23:31:16 +0000

    Draft main structure

--- main/src/main.rs
+++ main/src/main.rs
@@ -4,7 +4,7 @@ use std::{env, sync::Arc, net::{SocketAddr, IpAddr, Ipv4Addr}};
 
 use axum::{Router, routing::get, Extension};
 use dotenv::dotenv;
-use routes::user_handler;
+
 use sqlx::SqlitePool;
 
 #[tokio::main]
@@ -17,9 +17,16 @@ async fn main() {
         .await
         .expect("connection failed.");
     let shared_pool = Arc::new(pool);
+    use routes::{user_handler, storage_handler};
     let app = Router::new()
         .route("/users", get(user_handler))
+        .route("/storage", get(storage_handler))
         .layer(Extension(shared_pool));
+    let sa = "SERVICE_ACCOUNT";
+    match env::var(sa) {
+        Ok(val) => println!("credentials path] {}", val),
+        Err(error) => println!("credentials path not found. {}", error),
+    }
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     axum::Server::bind(&addr)
         .serve(app.into_make_service());
--- main/src/routes.rs
+++ main/src/routes.rs
@@ -1,19 +1,35 @@
 use std::sync::Arc;
 
 use axum::Json;
+
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{Extension, extract::Query};
+use cloud_storage::Client;
 use sqlx::SqlitePool;
 use model::UserQuery;
 use model::User;
 
-use crate::model;
+use crate::model::{self, StorageQuery, StorageResponse};
 
 pub async fn user_handler(Query(query):Query<UserQuery>, Extension(pool):Extension<Arc<SqlitePool>>) -> impl IntoResponse {
     let selected = query.user_id;
     match sqlx::query_as!(User, "select user_id, email_address, created_at, deleted, settings from users where user_id = ?", selected).fetch_optional(&*pool).await {
         Ok(user) => (StatusCode::OK, Json(user)),
         Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, Json(None::<User>)),
+    }
+}
+
+pub async fn storage_handler(Query(query):Query<StorageQuery>) -> impl IntoResponse {
+    let bucket = query.bucket;
+    let object = query.object;
+    let client = Client::default();
+    match client.object().download(&bucket, &object).await {
+        Ok(_) => {
+            (StatusCode::OK, axum::Json(None::<User>))
+        },
+        Err(_) => {
+            (StatusCode::OK, axum::Json(None::<User>))
+        },
     }
 }
