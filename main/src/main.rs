1f54c3793533ba5e8719430a650ff20fe3472b14
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-11-17 01:00:15 +0000

    Add DI to server

--- main/src/main.rs
+++ main/src/main.rs
@@ -7,29 +7,30 @@ use dotenv::dotenv;
 
 use sqlx::SqlitePool;
 
-use crate::routes::UserService;
+use crate::routes::{UserService, StorageService};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     let key = "DATABASE_URL";
     let db_url = env::var(key)
-        .expect("key not fount.");
+        .expect("Environment variable DATABASE_URL is not set.");
     let pool = SqlitePool::connect(&db_url)
         .await
-        .expect("connection failed.");
+        .expect("Failed to connect to database.");
 
     let sa = "SERVICE_ACCOUNT";
     match env::var(sa) {
-        Ok(val) => println!("credentials path] {}", val),
-        Err(error) => println!("credentials path not found. {}", error),
+        Ok(val) => println!("Environment Variable SERVICE_ACCOUNT is {}", val),
+        Err(error) => println!("Environment Variable SERVICE_ACCOUNT is not set. {}", error),
     }
     
     use routes::{user_handler, storage_handler};
     let user_service = Arc::new(UserService::new(pool));
+    let storage_service = Arc::new(StorageService::new());
     let app = Router::new()
         .route("/users", get(user_handler).layer(Extension(user_service)))
-        .route("/storage", get(storage_handler));
+        .route("/storage", get(storage_handler).layer(Extension(storage_service)));
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
